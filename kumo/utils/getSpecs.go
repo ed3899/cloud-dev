8df7ae8f4cc42579117d70b49abd4abbf7524d07
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-05 15:27:13 -0700

    checkpoint download

--- kumo/utils/getSpecs.go
+++ kumo/utils/getSpecs.go
@@ -1,14 +1,12 @@
 package utils
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"runtime"
-	"text/template"
 
 	"github.com/schollz/progressbar/v3"
 )
@@ -23,11 +21,19 @@ type Urls struct {
 	Pulumi string
 }
 
+type ZipExecutableRef struct {
+	URL     string
+	BinPath string
+}
+
 func init() {
 	hs := getHostSpecs()
 	packerUrl := getPackerUrl(hs)
-	downloadPacker(*packerUrl)
-	// downloadPackages(hs)
+	pulumiUrl := getPulumiUrl(hs)
+	urls := []*ZipExecutableRef{packerUrl, pulumiUrl}
+
+	// downloadPacker(*packerUrl)
+	downloadPackages(urls)
 }
 
 func getHostSpecs() Specs {
@@ -51,63 +57,89 @@ func validateHostCompatibility(s Specs) {
 	}
 }
 
-type PackerExe struct {
-	URL string
-	File string
+func getPackerUrl(s Specs) *ZipExecutableRef {
+	return &ZipExecutableRef{
+		URL:     fmt.Sprintf("https://releases.hashicorp.com/packer/1.9.1/packer_1.9.1_%s_%s.zip", s.OS, s.ARCH),
+		BinPath: fmt.Sprintf("packer_%s_%s.zip", s.OS, s.ARCH),
+	}
 }
 
-func getPackerUrl(s Specs) *PackerExe {
-	packerTemplateUrl := "https://releases.hashicorp.com/packer/1.9.1/packer_1.9.1_{{ .OS }}_{{ .ARCH }}.zip"
-	tmpl, err := template.New("packer-url").Parse(packerTemplateUrl)
-
-	if err != nil {
-		log.Fatalf("error parsing template: %s. Here is the error: %#v\n", tmpl.Name(), err)
+func getPulumiUrl(s Specs) *ZipExecutableRef {
+	var arch string
+	switch s.ARCH {
+	case "amd64":
+		arch = "x64"
 	}
 
-	var packerTemplate bytes.Buffer
-	err = tmpl.Execute(&packerTemplate, s)
-	if err != nil {
-		log.Fatalf("error executing template: %#v", err)
+	return &ZipExecutableRef{
+		URL:     fmt.Sprintf("https://get.pulumi.com/releases/sdk/pulumi-v3.74.0-%s-%s.zip", s.OS, arch),
+		BinPath: fmt.Sprintf("pulumi_%s_%s.zip", s.OS, s.ARCH),
 	}
-
-	return &PackerExe{
-		URL: packerTemplate.String(),
-		File: fmt.Sprintf("packer_%s_%s.zip", s.OS, s.ARCH),
-	}
-
 }
 
-func downloadPacker(p PackerExe) {
-	downloadFile(p.URL, p.File)
+// func downloadPacker(p ZipExecutableRef) {
+// 	downloadBin(p.URL, p.BinPath)
+// }
+
+func downloadPackages(ze []*ZipExecutableRef) {
+	resultChan := make(chan DownloadResult, 2)
+
+	for i := 0; i < len(ze); i++ {
+		go downloadBin(*ze[i], resultChan)
+	}
+
+	for i := 0; i < len(ze); i++ {
+		result := <-resultChan
+		if result.Err != nil {
+			log.Printf("Error downloading %s: %v\n", result.ZipRef.URL, result.Err)
+		} else {
+			fmt.Printf("Downloaded %s to %s successfully\n", result.ZipRef.URL, result.ZipRef.BinPath)
+		}
+	}
 }
 
-func downloadFile(url string, filePath string) error {
+type DownloadResult struct {
+	ZipRef *ZipExecutableRef
+	Err    error
+}
+
+func downloadBin(ref ZipExecutableRef, resultChan chan<- DownloadResult) {
+	err := download(ref.URL, ref.BinPath)
+
+	result := DownloadResult{
+		ZipRef: &ref,
+		Err:    err,
+	}
+
+	resultChan <- result
+}
+
+func download(url string, binPath string) error {
+	// Download
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("there was an error while attempting to download %#v", url)
 	}
 	defer response.Body.Close()
 
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0744)
+	// Create
+	file, err := os.OpenFile(binPath, os.O_CREATE|os.O_WRONLY, 0744)
 	if err != nil {
-		log.Fatalf("there was an error while creating %#v", filePath)
+		log.Fatalf("there was an error while creating %#v", binPath)
 	}
 	defer file.Close()
 
 	bar := progressbar.DefaultBytes(
 		response.ContentLength,
-		fmt.Sprintf("Downloading %s", filePath),
+		fmt.Sprintf("Downloading %s", binPath),
 	)
 
+	// Fill
 	_, err = io.Copy(io.MultiWriter(file, bar), response.Body)
 	if err != nil {
-		log.Fatalf("there was an error while copying contents to %#v", filePath)
+		log.Fatalf("there was an error while copying contents to %#v", binPath)
 		return err
 	}
 
 	return nil
-}
-
-func downloadPackages(s Specs) {
-
 }
