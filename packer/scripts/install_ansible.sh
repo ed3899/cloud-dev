6468d6c438aa25f5243ebf986631956bc08bb347
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-10 19:33:25 -0700

    moved packer dir

--- packer/scripts/install_ansible.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -uo pipefail  # Exit immediately if unset variables are referenced, or pipelines return errors
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible 
--- packer/scripts/update_and_upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -uo pipefail  # Exit immediately if unset variables are referenced, or pipelines return errors
-sudo apt-get update -qq
-sudo apt-get upgrade -qq
