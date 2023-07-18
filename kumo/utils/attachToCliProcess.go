b5c3db7bd50fe569d9f4f106b9cd2ea3f92194e2
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-18 11:54:37 -0700

    check!

--- kumo/utils/attachToCliProcess.go
+++ kumo/utils/attachToCliProcess.go
@@ -13,6 +13,7 @@ import (
 )
 
 func AttachCliToProcess(cmd *exec.Cmd) (err error) {
+	// Get StdoutPipe and StderrPipe
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		err = errors.Wrap(err, "Error occurred while getting StdoutPipe")
@@ -31,13 +32,17 @@ func AttachCliToProcess(cmd *exec.Cmd) (err error) {
 		return err
 	}
 
-	var mg sync.WaitGroup
+	// The wait group for all cmd related goroutines
+	var mcg sync.WaitGroup
+	// The error channel for all cmd related goroutines
 	cmdErrChan := make(chan error, 1)
-	cmdDone := make(chan bool, 1)
+	// The done channel for all cmd related goroutines
+	cmdGoRoutinesDone := make(chan bool, 1)
 
-	mg.Add(1)
+	// Copy command StdoutPipe to our Stdout
+	mcg.Add(1)
 	go func(src *io.ReadCloser, dest *os.File) {
-		defer mg.Done()
+		defer mcg.Done()
 		if _, err := io.Copy(dest, *src); err != nil {
 			errx := errors.Wrap(err, "Error occurred while copying StdoutPipe to Stdout")
 			cmdErrChan <- errx
@@ -45,9 +50,10 @@ func AttachCliToProcess(cmd *exec.Cmd) (err error) {
 		}
 	}(&stdout, os.Stdout)
 
-	mg.Add(1)
+	// Copy command StderrPipe to our Stderr
+	mcg.Add(1)
 	go func(src *io.ReadCloser, dest *os.File) {
-		defer mg.Done()
+		defer mcg.Done()
 		if _, err := io.Copy(dest, *src); err != nil {
 			errx := errors.Wrap(err, "Error occurred while copying StderrPipe to Stderr")
 			cmdErrChan <- errx
@@ -55,22 +61,27 @@ func AttachCliToProcess(cmd *exec.Cmd) (err error) {
 		}
 	}(&stderr, os.Stderr)
 
-	mg.Add(1)
+	// Wait for command to finish
+	mcg.Add(1)
 	go func() {
-		defer mg.Done()
+		defer mcg.Done()
 		if err := cmd.Wait(); err != nil {
 			err = errors.Wrap(err, "Error occurred while waiting for command to finish")
 			cmdErrChan <- err
+			cmdGoRoutinesDone <- false
+			return
 		}
 	}()
 
+	// Wait for all cmd related goroutines to finish
 	go func() {
-		defer close(cmdDone)
+		defer close(cmdGoRoutinesDone)
 		defer close(cmdErrChan)
-		mg.Wait()
-		cmdDone <- true
+		mcg.Wait()
+		cmdGoRoutinesDone <- true
 	}()
 
+	// The aggregator wait group
 	var sg sync.WaitGroup
 	var signalChan = make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
@@ -90,7 +101,7 @@ func AttachCliToProcess(cmd *exec.Cmd) (err error) {
 					return
 				}
 
-			case d := <-cmdDone:
+			case d := <-cmdGoRoutinesDone:
 				if d {
 					return
 				}
