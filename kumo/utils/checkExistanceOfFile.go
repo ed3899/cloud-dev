8b0891e49ad23f2c487a4c82bdf502f7ea9e2473
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-19 10:21:52 -0700

    added check existance of file tests

--- kumo/utils/checkExistanceOfFile.go
+++ kumo/utils/checkExistanceOfFile.go
@@ -4,17 +4,23 @@ import (
 	"os"
 )
 
+// Returns true if the file at the given absolute path exists, false otherwise.
 func FilePresent(absolutePath string) bool {
+	// Check if the file at the given absolute path exists
 	_, err := os.Stat(absolutePath)
+	// If there is no error, it means the file exists
 	if err == nil {
-		return true // File exists
+		return true
 	}
+	// If the error indicates that the file does not exist, return false
 	if os.IsNotExist(err) {
 		return false
 	}
+	// If there is any other type of error, assume the file does not exist
 	return false
 }
 
+// Returns true if the file at the given absolute path does not exist, false otherwise.
 func FileNotPresent(absolutePath string) bool {
 	return !FilePresent(absolutePath)
 }
