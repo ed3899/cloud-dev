1d7cb2d65225ab312e226f19c7b9ebbbb3d6abf5
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-22 18:14:20 -0700

    cheeck

--- kumo/utils/download.go
+++ kumo/utils/download.go
@@ -1,16 +1,63 @@
 package utils
 
 import (
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
 )
 
-func Download(url string, dest string) (err error) {
+func Download(url string, destName string) (bytesDownloadedChan chan int, err error) {
 	response, err := http.Get(url)
 	if err != nil {
 		err = errors.Wrapf(err, "failed to download from: %s", url)
 		return
 	}
 	defer response.Body.Close()
+
+	destDir := filepath.Dir(destName)
+
+	// Create the destination along with all the necessary directories
+	err = os.MkdirAll(destDir, 0755)
+	if err != nil {
+		err = errors.Wrapf(err, "failed to create destination directory for: %s", destName)
+		return
+	}
+
+	file, err := os.OpenFile(destName, os.O_CREATE|os.O_WRONLY, 0744)
+	if err != nil {
+		err = errors.Wrapf(err, "failed to create file for: %s", destName)
+		return
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 4096)
+
+	// Iterate over the response body and sned downloaded bytes to channel
+	for {
+		bytesDownloaded, err := response.Body.Read(buffer)
+
+		if err != nil && err != io.EOF {
+			err = errors.Wrap(err, "failed to read response body")
+			return nil, err
+		}
+
+		if bytesDownloaded == 0 {
+			break
+		}
+
+		bytesDownloadedChan <- bytesDownloaded
+
+		_, err = file.Write(buffer[:bytesDownloaded])
+
+		if err != nil {
+			err = errors.Wrap(err, "failed to write to file")
+			return nil, err
+		}
+
+	}
+
+	return
 }
