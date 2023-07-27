f5e22125296bb40396d438996f94184af72f9c81
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-26 20:41:49 -0700

    check

--- kumo/utils/getPublicIp.go
+++ kumo/utils/getPublicIp.go
@@ -2,9 +2,10 @@ package utils
 
 import (
 	"io"
+	"log"
 	"net/http"
 
-	"github.com/pkg/errors"
+	"github.com/samber/oops"
 )
 
 // Returns the public IP of the machine running the program.
@@ -17,23 +18,36 @@ import (
 //	() -> ("123.456.789.012", nil)
 func GetPublicIp() (ip string, err error) {
 	var (
+		oopsBuilder = oops.Code("get_public_ip_failed")
+		URL         = "https://api.ipify.org?format=text"
+
 		response      *http.Response
 		bytesResponse []byte
 	)
-	
+
 	// Send GET request to retrieve public IP
-	if response, err = http.Get("https://api.ipify.org?format=text"); err != nil {
-		return "", errors.Wrap(err, "Error occurred while getting public IP")
+	if response, err = http.Get(URL); err != nil {
+		err = oopsBuilder.
+			Wrapf(err, "Error occurred while sending GET request to '%s'", URL)
+		return
 	}
 	defer func() {
-		if errClosingBody := response.Body.Close(); err != nil {
-			err = errors.Wrap(errClosingBody, "Error occurred while closing response body")
+		if err := response.Body.Close(); err != nil {
+			log.Fatalf(
+				"%+v",
+				oopsBuilder.
+					With("responseStatusCode", response.StatusCode).
+					Wrapf(err, "Error occurred while closing response body"),
+			)
 		}
 	}()
 
 	// Read the response body
 	if bytesResponse, err = io.ReadAll(response.Body); err != nil {
-		return "", errors.Wrap(err, "Error occurred while reading response body")
+		err = oopsBuilder.
+			With("responseStatusCode", response.StatusCode).
+			Wrapf(err, "Error occurred while reading response body")
+		return
 	}
 
 	// Convert the response body to a string
