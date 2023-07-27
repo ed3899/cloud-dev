5cbbaa441727b627cac146ffc4f0118168cf828e
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-27 12:34:07 -0700

    check

--- kumo/binaries/download/downloadAndExtract.go
+++ kumo/binaries/download/downloadAndExtract.go
@@ -5,7 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/ed3899/kumo/binaries"
-	"github.com/pkg/errors"
+	"github.com/samber/oops"
 	"github.com/vbauerster/mpb/v8"
 )
 
@@ -13,32 +13,47 @@ func DownloadAndExtract(z binaries.ZipI, extractToAbsPathDir string) (err error)
 	var (
 		absPathToZipDir = filepath.Dir(z.GetPath())
 		progress        = mpb.New(mpb.WithWidth(64), mpb.WithAutoRefresh())
+		oopsBuilder     = oops.
+				Code("download_and_extract_failed").
+				With("extractToAbsPathDir", extractToAbsPathDir).
+				With("z.GetName()", z.GetName()).
+				With("z.GetPath()", z.GetPath())
 	)
 
 	// Start with a clean slate
 	if err = os.RemoveAll(extractToAbsPathDir); err != nil {
-		return errors.Wrapf(err, "Error occurred while removing %s", extractToAbsPathDir)
+		err = oopsBuilder.
+			Wrapf(err, "Error occurred while removing %s", extractToAbsPathDir)
+		return
 	}
 
 	if err = os.RemoveAll(absPathToZipDir); err != nil {
-		return errors.Wrapf(err, "Error occurred while removing %s", absPathToZipDir)
+		err = oopsBuilder.
+			Wrapf(err, "Error occurred while removing %s", absPathToZipDir)
+		return
 	}
 
 	// Download
 	if err = DownloadAndShowProgress(z, progress); err != nil {
-		return errors.Wrapf(err, "Error occurred while downloading %s", z.GetName())
+		err = oopsBuilder.
+			Wrapf(err, "Error occurred while downloading %s", z.GetName())
+		return
 	}
 
 	// Extract
 	if err = ExtractAndShowProgress(z, extractToAbsPathDir, progress); err != nil {
-		return errors.Wrapf(err, "Error occurred while extracting %s", z.GetName())
+		err = oopsBuilder.
+			Wrapf(err, "Error occurred while extracting %s", z.GetName())
+		return
 	}
 
 	progress.Shutdown()
 
 	// Remove zip
 	if err = z.Remove(); err != nil {
-		return errors.Wrapf(err, "Error occurred while removing %s", z.GetName())
+		err = oopsBuilder.
+			Wrapf(err, "Error occurred while removing %s", z.GetName())
+		return
 	}
 
 	return
