7710cb2ded7ce90001073a46279e714cb39a7773
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-27 12:30:28 -0700

    check

--- kumo/binaries/download/downloadAndShowProgress.go
+++ kumo/binaries/download/downloadAndShowProgress.go
@@ -1,9 +1,8 @@
 package download
 
 import (
-
 	"github.com/ed3899/kumo/binaries"
-	"github.com/pkg/errors"
+	"github.com/samber/oops"
 	"github.com/vbauerster/mpb/v8"
 )
 
@@ -16,12 +15,16 @@ type DownloadableAndProgressive interface {
 func DownloadAndShowProgress[D DownloadableAndProgressive](d D, multiProgressBar *mpb.Progress) (err error) {
 	var (
 		downloadedBytesChan = make(chan int, 1024)
-		downloadedBytes     int
+		errChan             = make(chan error, 1)
+		doneChan            = make(chan bool, 1)
+		oopsBuilder         = oops.
+					Code("download_and_show_progress_failed").
+					With("d.GetName()", d.GetName()).
+					With("d.GetPath()", d.GetPath()).
+					With("multiProgressBar", multiProgressBar)
 
-		errChan = make(chan error, 1)
-
-		doneChan = make(chan bool, 1)
-		done     bool
+		downloadedBytes int
+		done            bool
 	)
 
 	go func() {
@@ -30,10 +33,15 @@ func DownloadAndShowProgress[D DownloadableAndProgressive](d D, multiProgressBar
 		defer close(doneChan)
 
 		d.SetDownloadBar(multiProgressBar)
-		if err = d.Download(downloadedBytesChan); err != nil {
+
+		if err := d.Download(downloadedBytesChan); err != nil {
+			err = oopsBuilder.
+				With("downloadedBytesChan", downloadedBytesChan).
+				Wrapf(err, "Error occurred while downloading %s", d.GetName())
 			errChan <- err
 			return
 		}
+
 		doneChan <- true
 	}()
 
@@ -41,29 +49,21 @@ OuterLoop:
 	for {
 		select {
 		case downloadedBytes = <-downloadedBytesChan:
-			if downloadedBytes <= 0 {
-				continue OuterLoop
+			if downloadedBytes > 0 {
+				d.IncrementDownloadBar(downloadedBytes)
 			}
-
-			d.IncrementDownloadBar(downloadedBytes)
 
 		case err = <-errChan:
-			if err == nil {
-				continue OuterLoop
+			if err != nil {
+				err = oopsBuilder.
+					Wrapf(err, "Error occurred while downloading %s", d.GetName())
+				return
 			}
-
-			if err = d.Remove(); err != nil {
-				err = errors.Wrapf(err, "Error occurred while removing %s", d.GetName())
-			}
-
-			err = errors.Wrapf(err, "Error occurred while downloading %s", d.GetName())
-			break OuterLoop
 
 		case done = <-doneChan:
 			if done {
 				break OuterLoop
 			}
-			continue OuterLoop
 		}
 	}
 
