ec2228b77cb7095c45ddc5c8ba3696e1d9825699
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-26 20:52:30 -0700

    check

--- kumo/utils/getContentLength.go
+++ kumo/utils/getContentLength.go
@@ -1,22 +1,33 @@
 package utils
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/pkg/errors"
+	"github.com/samber/oops"
 )
 
 func GetContentLength(url string) (contentLength int64, err error) {
 	var (
+		oopsBuilder = oops.Code("get_content_length_failed").
+				With("url", url)
+
 		response *http.Response
 	)
 
 	if response, err = http.Head(url); err != nil {
-		return 0, errors.Wrap(err, "failed to get head response")
+		err = oopsBuilder.
+			Wrapf(err, "failed to get head response from: %s", url)
+		return
 	}
 	defer func() {
-		if errClosingBody := response.Body.Close(); err != nil {
-			err = errors.Wrap(errClosingBody, "failed to close response body")
+		if err := response.Body.Close(); err != nil {
+			log.Fatalf(
+				"%+v",
+				oopsBuilder.
+					With("responseStatus", response.Status).
+					Wrapf(err, "failed to close response body"),
+			)
 		}
 	}()
 
