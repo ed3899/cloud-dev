27f112d32a8e73c81ec523b129f9430d1ebf8a9a
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-29 15:52:11 -0700

    check

--- kumo/vars/packer/aws/instance.go
+++ kumo/vars/packer/aws/instance.go
@@ -2,29 +2,48 @@ package aws
 
 import (
 	"os"
+	"path/filepath"
 
+	"github.com/ed3899/kumo/common/dirs"
+	"github.com/ed3899/kumo/common/hashicorp_vars"
 	"github.com/samber/oops"
 )
 
-type Vars struct {
+type HashicorpVars struct {
 	file *os.File
 }
 
-func NewVars() (vars *Vars, err error) {
+func NewVars() (hashicorpVars *HashicorpVars, err error) {
 	var (
 		oopsBuilder = oops.
-			Code("new_vars_failed")
+				Code("new_vars_failed")
 
-		varsFile *os.File
+		varsFile          *os.File
+		absPathToVarsFile string
 	)
 
-	if varsFile, err = os.Create(hv.AbsPath); err != nil {
-		return errors.Wrapf(err, "Error occurred while creating %s", hv.AbsPath)
+	if absPathToVarsFile, err = filepath.Abs(filepath.Join(dirs.PACKER_DIR_NAME, dirs.AWS_DIR_NAME, hashicorp_vars.PACKER_VARS_NAME)); err != nil {
+		err = oopsBuilder.
+			With("dirs.PACKER_DIR_NAME", dirs.PACKER_DIR_NAME).
+			With("dirs.AWS_DIR_NAME", dirs.AWS_DIR_NAME).
+			Wrapf(err, "Error occurred while crafting absolute path to %s", hashicorp_vars.PACKER_VARS_NAME)
+		return
+	}
+
+	if varsFile, err = os.Create(absPathToVarsFile); err != nil {
+		err = oopsBuilder.
+			With("absPathToVarsFile", absPathToVarsFile).
+			Wrapf(err, "Error occurred while creating %s", hashicorp_vars.PACKER_VARS_NAME)
+		return
+	}
+
+	hashicorpVars = &HashicorpVars{
+		file: varsFile,
 	}
 
 	return
 }
 
-func (v *Vars) GetFile() (file *os.File) {
-	return v.file
+func (hv *HashicorpVars) GetFile() (file *os.File) {
+	return hv.file
 }
