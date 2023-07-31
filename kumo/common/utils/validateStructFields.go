bcc12942434fcc23455ef23e9cf69ed62ef2cb00
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-31 15:35:11 -0700

    check

--- kumo/common/utils/validateStructFields.go
+++ kumo/common/utils/validateStructFields.go
@@ -13,6 +13,14 @@ func IsStructCompletellyFilled(s any) (isFilledCompletely bool) {
 	for i := 0; i < reflectValue.NumField(); i++ {
 		fieldValue = reflectValue.Field(i)
 
+		// Check for []string
+		if fieldValue.Kind() == reflect.Slice && fieldValue.Type().Elem().Kind() == reflect.String {
+			if fieldValue.Len() == 0 {
+				return false
+			}
+			continue
+		}
+
 		// Check if the field is uninitialized (has zero value)
 		if fieldValue.Interface() == reflect.Zero(fieldValue.Type()).Interface() {
 			return false // Return false if any field is undefined
