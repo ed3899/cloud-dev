2f81fadad98d00cf1a9f95a7a8549f1c08150631
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-31 15:03:33 -0700

    check

--- kumo/common/utils/host.go
+++ kumo/common/utils/host.go
@@ -1,6 +1,8 @@
 package utils
 
-import "runtime"
+import (
+	"runtime"
+)
 
 func GetCurrentHostSpecs() (os, arch string) {
 	return runtime.GOOS, runtime.GOARCH
@@ -10,14 +12,31 @@ func GetCurrentHostSpecs() (os, arch string) {
 func HostIsCompatible() (compatible bool) {
 	switch runtime.GOOS {
 	case "windows":
+
 		switch runtime.GOARCH {
 		case "386":
 			compatible = true
+
 		case "amd64":
 			compatible = true
+
 		default:
 			compatible = false
 		}
+
+	case "darwin":
+
+		switch runtime.GOARCH {
+		case "amd64":
+			compatible = true
+
+		case "arm64":
+			compatible = true
+
+		default:
+			compatible = false
+		}
+
 	default:
 		compatible = false
 	}
