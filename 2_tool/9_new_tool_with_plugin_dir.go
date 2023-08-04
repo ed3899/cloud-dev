ba86ff3fd2f5cef6ed8c91bf3181d4b748dc8e31
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-04 15:33:33 -0700

    check

---
+++ 2_tool/9_new_tool_with_plugin_dir.go
@@ -0,0 +1,59 @@
+package tool
+
+import (
+	"path/filepath"
+
+	constants "github.com/ed3899/kumo/0_constants"
+	cloud "github.com/ed3899/kumo/1_cloud"
+	"github.com/ed3899/kumo/common/dirs"
+	"github.com/samber/oops"
+)
+
+type ToolWithPluginDir struct {
+	ToolWithRunDir
+	PluginsDir string
+}
+
+func NewToolWithPluginDir(
+	toolKind constants.ToolKind,
+	toolWithRunDir ToolWithRunDir,
+	cloud cloud.Cloud,
+	kumoExecutableAbsPath string,
+) (toolWithPluginDir ToolWithPluginDir, err error) {
+	var (
+		oopsBuilder = oops.Code(
+			"new_tool_with_plugin_dir_failed",
+		)
+	)
+
+	switch toolKind {
+	case constants.Packer:
+		toolWithPluginDir = ToolWithPluginDir{
+			ToolWithRunDir: toolWithRunDir,
+			PluginsDir: filepath.Join(
+				kumoExecutableAbsPath,
+				constants.PACKER,
+				cloud.Name,
+				dirs.PLUGINS_DIR_NAME,
+			),
+		}
+
+	case constants.Terraform:
+		toolWithPluginDir = ToolWithPluginDir{
+			ToolWithRunDir: toolWithRunDir,
+			PluginsDir: filepath.Join(
+				kumoExecutableAbsPath,
+				constants.TERRAFORM,
+				cloud.Name,
+				dirs.PLUGINS_DIR_NAME,
+			),
+		}
+
+	default:
+		err = oopsBuilder.
+			Errorf("Unknown tool kind: %d", toolKind)
+		return
+	}
+
+	return
+}
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/oklog/ulid/v2 v2.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/samber/mo v1.8.0 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
