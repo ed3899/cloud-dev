801363205976eca0210a34e7470b97026a42565e
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-07 13:49:52 -0700

    check

--- tool/environment/packer/general/new_packer_general_environment.go
+++ tool/environment/packer/general/new_packer_general_environment.go
@@ -3,23 +3,22 @@ package general
 import (
 	"github.com/ed3899/kumo/utils/environment"
 	"github.com/samber/oops"
-	"github.com/spf13/viper"
 )
 
-func NewEnvironment() (generalEnvironment PackerGeneralEnvironment) {
-	generalEnvironment = PackerGeneralEnvironment{
-		Required: PackerGeneralRequired{
-			GIT_USERNAME: viper.GetString("Git.Username"),
-			GIT_EMAIL:    viper.GetString("Git.Email"),
-			ANSIBLE_TAGS: viper.GetStringSlice("AMI.Tools"),
-		},
-		Optional: PackerGeneralOptional{
-			GIT_HUB_PERSONAL_ACCESS_TOKEN_CLASSIC: viper.GetString("GitHub.PersonalAccessTokenClassic"),
-		},
-	}
+// func NewEnvironment() (generalEnvironment PackerGeneralEnvironment) {
+// 	generalEnvironment = PackerGeneralEnvironment{
+// 		Required: PackerGeneralRequired{
+// 			GIT_USERNAME: viper.GetString("Git.Username"),
+// 			GIT_EMAIL:    viper.GetString("Git.Email"),
+// 			ANSIBLE_TAGS: viper.GetStringSlice("AMI.Tools"),
+// 		},
+// 		Optional: PackerGeneralOptional{
+// 			GIT_HUB_PERSONAL_ACCESS_TOKEN_CLASSIC: viper.GetString("GitHub.PersonalAccessTokenClassic"),
+// 		},
+// 	}
 
-	return
-}
+// 	return
+// }
 
 func NewPackerGeneralEnvironment(
 	areRequiredFieldsNotFilled environment.IsStructNotCompletelyFilledF,
@@ -52,6 +51,38 @@ func NewPackerGeneralEnvironment(
 	return
 }
 
+func WithGitUsername(gitUsername string) (option Option) {
+	option = func(environment *PackerGeneralEnvironment) {
+		environment.Required.GIT_USERNAME = gitUsername
+	}
+
+	return
+}
+
+func WithGitEmail(gitEmail string) (option Option) {
+	option = func(environment *PackerGeneralEnvironment) {
+		environment.Required.GIT_EMAIL = gitEmail
+	}
+
+	return
+}
+
+func WithAnsibleTags(ansibleTags []string) (option Option) {
+	option = func(environment *PackerGeneralEnvironment) {
+		environment.Required.ANSIBLE_TAGS = ansibleTags
+	}
+
+	return
+}
+
+func WithGitHubPersonalAccessTokenClassic(gitHubPersonalAccessTokenClassic string) (option Option) {
+	option = func(environment *PackerGeneralEnvironment) {
+		environment.Optional.GIT_HUB_PERSONAL_ACCESS_TOKEN_CLASSIC = gitHubPersonalAccessTokenClassic
+	}
+
+	return
+}
+
 type PackerGeneralRequired struct {
 	GIT_USERNAME string
 	GIT_EMAIL    string
@@ -67,4 +98,4 @@ type PackerGeneralEnvironment struct {
 	Optional *PackerGeneralOptional
 }
 
-type Option func(environment *PackerGeneralEnvironment) (err error)
+type Option func(environment *PackerGeneralEnvironment)
