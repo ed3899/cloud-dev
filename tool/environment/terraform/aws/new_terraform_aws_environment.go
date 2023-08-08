fcf6664b601e651600e459781688ab02baac5e22
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-07 17:05:07 -0700

    check

--- tool/environment/terraform/aws/new_terraform_aws_environment.go
+++ tool/environment/terraform/aws/new_terraform_aws_environment.go
@@ -1,53 +1,44 @@
 package aws
 
-import (
-	"path/filepath"
+// func NewTerraformAwsEnvironment(
+// 	getLastAmiIdFromPackerManifest packer_manifest.GetLastBuiltAmiIdFromPackerManifestF,
+// 	pickAmiId packer_manifest.PickAmiIdF,
+// 	kumoExecAbsPath string,
+// ) (environment TerraformAwsEnvironment) {
+// 	var (
+// 		oopsBuilder = oops.
+// 				Code("NewEnvironment").
+// 				With("kumoExecAbsPath", kumoExecAbsPath).
+// 				With("getLastAmiIdFromPackerManifest", getLastAmiIdFromPackerManifest).
+// 				With("pickAmiId", pickAmiId)
+// 		packerManifestAbsPath = filepath.Join(
+// 			kumoExecAbsPath,
+// 			constants.PACKER,
+// 			constants.AWS,
+// 			constants.PACKER_MANIFEST,
+// 		)
 
-	"github.com/ed3899/kumo/constants"
-	"github.com/ed3899/kumo/utils/packer_manifest"
-	"github.com/samber/oops"
-	"github.com/spf13/viper"
-)
+// 		lastAmiId string
+// 	)
 
-func NewTerraformAwsEnvironment(
-	getLastAmiIdFromPackerManifest packer_manifest.GetLastBuiltAmiIdFromPackerManifestF,
-	pickAmiId packer_manifest.PickAmiIdF,
-	kumoExecAbsPath string,
-) (environment TerraformAwsEnvironment) {
-	var (
-		oopsBuilder = oops.
-				Code("NewEnvironment").
-				With("kumoExecAbsPath", kumoExecAbsPath).
-				With("getLastAmiIdFromPackerManifest", getLastAmiIdFromPackerManifest).
-				With("pickAmiId", pickAmiId)
-		packerManifestAbsPath = filepath.Join(
-			kumoExecAbsPath,
-			constants.PACKER,
-			constants.AWS,
-			constants.PACKER_MANIFEST,
-		)
+// 	environment = TerraformAwsEnvironment{
+// 		Required: TerraformAwsRequired{
+// 			AWS_REGION:        viper.GetString("AWS.Region"),
+// 			AWS_INSTANCE_TYPE: viper.GetString("AWS.EC2.Instance.Type"),
+// 			AMI_ID:            pickedAmiId,
+// 			KEY_NAME:          constants.KEY_NAME,
+// 			SSH_PORT:          constants.SSH_PORT,
+// 			IP_FILE_NAME:      constants.IP_FILE_NAME,
+// 			USERNAME:          viper.GetString("AMI.User"),
+// 		},
+// 		Optional: TerraformAwsOptional{
+// 			AWS_EC2_INSTANCE_VOLUME_TYPE: viper.GetString("AWS.EC2.Volume.Type"),
+// 			AWS_EC2_INSTANCE_VOLUME_SIZE: viper.GetInt("AWS.EC2.Volume.Size"),
+// 		},
+// 	}
 
-		lastAmiId string
-	)
-
-	environment = TerraformAwsEnvironment{
-		Required: TerraformAwsRequired{
-			AWS_REGION:        viper.GetString("AWS.Region"),
-			AWS_INSTANCE_TYPE: viper.GetString("AWS.EC2.Instance.Type"),
-			AMI_ID:            pickedAmiId,
-			KEY_NAME:          constants.KEY_NAME,
-			SSH_PORT:          constants.SSH_PORT,
-			IP_FILE_NAME:      constants.IP_FILE_NAME,
-			USERNAME:          viper.GetString("AMI.User"),
-		},
-		Optional: TerraformAwsOptional{
-			AWS_EC2_INSTANCE_VOLUME_TYPE: viper.GetString("AWS.EC2.Volume.Type"),
-			AWS_EC2_INSTANCE_VOLUME_SIZE: viper.GetInt("AWS.EC2.Volume.Size"),
-		},
-	}
-
-	return
-}
+// 	return
+// }
 
 type TerraformAwsRequired struct {
 	AWS_REGION        string
--- tool/environment/terraform/general/new_terraform_general_environment.go
+++ tool/environment/terraform/general/new_terraform_general_environment.go
@@ -1,46 +1,44 @@
 package general
 
 import (
-	"github.com/ed3899/kumo/constants"
 	"github.com/ed3899/kumo/utils/ip"
-	"go.uber.org/zap"
 )
 
-func NewEnvironment(
-	getPublicIp ip.GetPublicIpF,
-	withMask ip.WithMaskF,
-) (
-	generalEnvironment TerraformGeneralEnvironment,
-) {
-	var (
-		logger, _ = zap.NewProduction()
+// func NewEnvironment(
+// 	getPublicIp ip.GetPublicIpF,
+// 	withMask ip.WithMaskF,
+// ) (
+// 	generalEnvironment TerraformGeneralEnvironment,
+// ) {
+// 	var (
+// 		logger, _ = zap.NewProduction()
 
-		err      error
-		publicIp string
-		pickedIp string
-	)
+// 		err      error
+// 		publicIp string
+// 		pickedIp string
+// 	)
 
-	defer logger.Sync()
+// 	defer logger.Sync()
 
-	if publicIp, err = getPublicIp(); err != nil {
-		logger.Warn(
-			"There was an error getting the public ip, using default instead",
-			zap.String("error", err.Error()),
-			zap.String("defaultIp", constants.TERRAFORM_DEFAULT_ALLOWED_IP),
-		)
-		pickedIp = constants.TERRAFORM_DEFAULT_ALLOWED_IP
-	} else {
-		pickedIp = publicIp
-	}
+// 	if publicIp, err = getPublicIp(); err != nil {
+// 		logger.Warn(
+// 			"There was an error getting the public ip, using default instead",
+// 			zap.String("error", err.Error()),
+// 			zap.String("defaultIp", constants.TERRAFORM_DEFAULT_ALLOWED_IP),
+// 		)
+// 		pickedIp = constants.TERRAFORM_DEFAULT_ALLOWED_IP
+// 	} else {
+// 		pickedIp = publicIp
+// 	}
 
-	generalEnvironment = TerraformGeneralEnvironment{
-		Required: TerraformGeneralRequired{
-			ALLOWED_IP: withMask(32)(pickedIp),
-		},
-	}
+// 	generalEnvironment = TerraformGeneralEnvironment{
+// 		Required: TerraformGeneralRequired{
+// 			ALLOWED_IP: withMask(32)(pickedIp),
+// 		},
+// 	}
 
-	return
-}
+// 	return
+// }
 
 type TerraformGeneralRequired struct {
 	ALLOWED_IP string
