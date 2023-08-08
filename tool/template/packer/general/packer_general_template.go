d4efc09e97c5590499de89951a69e9c598604507
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-08 10:21:58 -0700

    check

---
+++ tool/template/packer/general/packer_general_template.go
@@ -0,0 +1,36 @@
+package general
+
+import (
+	"path/filepath"
+	"text/template"
+
+	"github.com/ed3899/kumo/constants"
+	"github.com/samber/oops"
+)
+
+func LoadPackerGeneralVarsTemplate(
+	kumoExecAbsPath string,
+) (
+	packerGeneralTemplate *template.Template,
+	err error,
+) {
+	var (
+		oopsBuilder = oops.
+				Code("LoadPackerGeneralTemplate")
+
+		packerGeneralTemplateAbsPath = filepath.Join(
+			kumoExecAbsPath,
+			constants.TEMPLATES_DIR_NAME,
+			constants.PACKER,
+			constants.PACKER_GENERAL_VARS_TEMPLATE,
+		)
+	)
+
+	if packerGeneralTemplate, err = template.ParseFiles(packerGeneralTemplateAbsPath); err != nil {
+		err = oopsBuilder.
+			Wrapf(err, "Failed to parse packer general template '%s'", packerGeneralTemplateAbsPath)
+		return
+	}
+
+	return
+}
