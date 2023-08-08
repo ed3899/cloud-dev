1de5d20f5d51606cf84f5ae366d1fe1f584a4984
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-08 14:46:11 -0700

    check

--- tool_manager/vars/packer/new_packer_vars.go
+++ tool_manager/vars/packer/new_packer_vars.go
@@ -1,4 +1,35 @@
 package packer
 
+import (
+	environment_packer "github.com/ed3899/kumo/tool_manager/environment/packer"
+	template_packer "github.com/ed3899/kumo/tool_manager/template/packer"
+)
+
 type PackerVars struct {
+	Environment *environment_packer.PackerEnvironment
+	Template    *template_packer.PackerTemplateCombo
 }
+
+func NewPackerVars(
+	options ...Option,
+) (
+	packerVars *PackerVars,
+) {
+	var (
+		option Option
+	)
+
+	packerVars = &PackerVars{}
+
+	for _, option = range options {
+		option(packerVars)
+	}
+
+	return
+}
+
+func (pv *PackerVars) OutputVars() (err error) {
+	return
+}
+
+type Option func(*PackerVars)
