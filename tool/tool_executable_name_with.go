853949f950585a4db5ca62f342ecdfcfec0a94a7
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-10 11:15:46 -0700

    check

--- tool/tool_executable_name_with.go
+++ tool/tool_executable_name_with.go
@@ -1,8 +1,18 @@
 package tool
 
-func ToolExecutableNameWith(
-	fmt_Sprintf func(format string, a ...any) string,
-	ToolName func() string,
-) string {
-	return fmt_Sprintf("%s.exe", ToolName())
+func ToolExecutableNameWith[
+	Fmt_Sprintf ~func(format string, a ...any) string,
+	ToolName ~func() string,
+](
+	fmt_Sprintf Fmt_Sprintf,
+	toolName ToolName,
+) ToolExecutableName {
+	return func() string {
+		return fmt_Sprintf(
+			"%s.exe",
+			toolName(),
+		)
+	}
 }
+
+type ToolExecutableName func() string
