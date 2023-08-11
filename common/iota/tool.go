63fad1798b69966d0d0187ab096263b3f67e11ce
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-11 15:21:52 -0700

    check

--- common/iota/tool.go
+++ common/iota/tool.go
@@ -1,5 +1,7 @@
 package iota
 
+import "github.com/samber/oops"
+
 type Tool int
 
 const (
@@ -8,45 +10,85 @@ const (
 )
 
 func (t Tool) Name() string {
-	switch t {
-	case Packer:
-		return "packer"
-	case Terraform:
-		return "terraform"
-	default:
-		panic("Unknown tool")
-	}
+	var choice string
+
+	oops.
+		In("iota").
+		Tags("tool").
+		Code("Name").
+		Recoverf(func() {
+			switch t {
+			case Packer:
+				choice = "packer"
+			case Terraform:
+				choice = "terraform"
+			default:
+				panic(t)
+			}
+		}, "Unknown tool")
+
+	return choice
 }
 
 func (t Tool) VarsName() string {
-	switch t {
-	case Packer:
-		return ".auto.pkrvars.hcl"
-	case Terraform:
-		return ".auto.tfvars"
-	default:
-		panic("Unknown tool")
-	}
+	var choice string
+
+	oops.
+		In("iota").
+		Tags("tool").
+		Code("VarsName").
+		Recoverf(func() {
+			switch t {
+			case Packer:
+				choice = ".auto.pkrvars.hcl"
+			case Terraform:
+				choice = ".auto.tfvars"
+			default:
+				panic(t)
+			}
+		}, "Unknown tool")
+
+	return choice
 }
 
 func (t Tool) Version() string {
-	switch t {
-	case Packer:
-		return "1.6.5"
-	case Terraform:
-		return "1.5.3"
-	default:
-		panic("Unknown tool")
-	}
+	var choice string
+
+	oops.
+		In("iota").
+		Tags("tool").
+		Code("Version").
+		Recoverf(func() {
+			switch t {
+			case Packer:
+				choice = "1.6.5"
+			case Terraform:
+				choice = "1.5.3"
+			default:
+				panic(t)
+			}
+		}, "Unknown tool")
+
+	return choice
 }
 
 func (t Tool) PluginPathEnvironmentVariable() string {
-	switch t {
-	case Packer:
-		return "PACKER_PLUGIN_PATH"
-	case Terraform:
-		return "TF_PLUGIN_CACHE_DIR"
-	default:
-		panic("Unknown tool")
-	}
+	var choice string
+
+	oops.
+		In("iota").
+		Tags("tool").
+		Code("PluginPathEnvironmentVariable").
+		Recoverf(func() {
+			switch t {
+			case Packer:
+				choice = "PACKER_PLUGIN_PATH"
+			case Terraform:
+				choice = "TF_PLUGIN_CACHE_DIR"
+			default:
+				panic(t)
+			}
+		}, "Unknown tool")
+
+	return choice
 }
