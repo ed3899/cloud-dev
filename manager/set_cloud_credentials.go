ff267a3fb477d43d10988798dfc065d2770654d6
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-14 12:16:32 -0700

    check

--- manager/set_cloud_credentials.go
+++ manager/set_cloud_credentials.go
@@ -5,33 +5,36 @@ import (
 	"github.com/samber/oops"
 )
 
-func SetCloudCredentials(
+func SetCloudCredentialsWith(
 	osSetenv func(string, string) error,
 	manager ICloudGetter[iota.Cloud],
-) error {
+) SetCloudCredentials {
 	oopsBuilder := oops.
 		In("manager").
 		Tags("Manager").
 		Code("SetCloudCredentials")
 
-	managerCloudName := manager.Cloud().Name()
-
-	switch manager.Cloud() {
-	case iota.Aws:
-		for key, value := range awsCredentials {
-			if err := osSetenv(key, value); err != nil {
-				return oopsBuilder.
-					With("cloudName", managerCloudName).
-					Wrapf(err, "failed to set environment variable %s to %s", key, value)
+	setCloudCredentials := func(manager ICloudGetter[iota.Cloud]) error {
+		switch manager.Cloud() {
+		case iota.Aws:
+			for key, value := range awsCredentials {
+				if err := osSetenv(key, value); err != nil {
+					return oopsBuilder.
+						With("cloudName", manager.Cloud().Name()).
+						Wrapf(err, "failed to set environment variable %s to %s", key, value)
+				}
 			}
+
+		default:
+			return oopsBuilder.
+				With("cloudName", manager.Cloud().Name()).
+				Errorf("unknown cloud: %#v", manager.Cloud())
 		}
 
-	default:
-		return oopsBuilder.
-			With("cloudName", managerCloudName).
-			Errorf("unknown cloud: %#v", manager.Cloud())
+		return nil
 	}
 
-	return nil
-
+	return setCloudCredentials
 }
+
+type SetCloudCredentials func(ICloudGetter[iota.Cloud]) error
