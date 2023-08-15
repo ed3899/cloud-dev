9eddc62665b6ca73d29a7abe868a3a49aabb5129
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-15 12:59:02 -0700

    check

---
+++ utils/file/is_file_present.go
@@ -0,0 +1,23 @@
+package file
+
+import (
+	"os"
+)
+
+// Returns true if the file at the given os.File exists, false otherwise.
+func IsFilePresent(absolutePath string) bool {
+	// Check if the file at the given absolute path exists
+	_, err := os.Stat(absolutePath)
+	// If there is no error, it means the file exists
+	if err == nil {
+		return true
+	}
+
+	// If the error indicates that the file does not exist, return false
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	// If there is any other type of error, assume the file does not exist
+	return false
+}
