9e9109d63a93766e00bfbbf07d1fbf5ef1681e3e
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 13:02:37 -0700

    check

---
+++ manager/create_vars.go
@@ -0,0 +1,21 @@
+package manager
+
+import (
+	"os"
+
+	"github.com/samber/oops"
+)
+
+func (m *Manager) CreateVars() (*os.File, error) {
+	oopsBuilder := oops.
+		Code("CreateVars")
+
+	file, err := os.Create(m.Path.Vars)
+	if err != nil {
+		return nil, oopsBuilder.
+			Wrapf(err, "failed to create vars file: %s", m.Path.Vars)
+	}
+	defer file.Close()
+
+	return file, nil
+}
