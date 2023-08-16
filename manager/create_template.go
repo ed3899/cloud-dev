41656b09311280f1060fd3dac59a9ca55510166c
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 12:54:21 -0700

    check

---
+++ manager/create_template.go
@@ -0,0 +1,19 @@
+package manager
+
+import (
+	"github.com/ed3899/kumo/utils/file"
+	"github.com/samber/oops"
+)
+
+func (m *Manager) CreateTemplate() error {
+	oopsBuilder := oops.
+		In("manager").
+		Code("Create")
+
+	err := file.MergeFilesTo(m.Path.Template.Merged, m.Path.Template.Cloud, m.Path.Template.Base)
+	if err != nil {
+		return oopsBuilder.Wrapf(err, "failed to merge files")
+	}
+
+	return nil
+}
---
+++ manager/parse_template.go
@@ -0,0 +1,20 @@
+package manager
+
+import (
+	"text/template"
+
+	"github.com/samber/oops"
+)
+
+func (m *Manager) ParseTemplate() (*template.Template, error) {
+	oopsBuilder := oops.
+		In("manager").
+		Code("ParseTemplate")
+
+	template, err := template.ParseFiles(m.Path.Template.Merged)
+	if err != nil {
+		return nil, oopsBuilder.Wrapf(err, "failed to parse template")
+	}
+
+	return template, nil
+}
