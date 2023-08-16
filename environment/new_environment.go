bf6d384962c7de138327a785c917a59fa20d05e9
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 08:42:46 -0700

    check

--- environment/new_environment.go
+++ environment/new_environment.go
@@ -1,14 +1,29 @@
 package environment
 
-import "github.com/ed3899/kumo/common/iota"
+import (
+	"github.com/ed3899/kumo/common/iota"
+	"github.com/samber/oops"
+)
 
 func NewEnvironment(
 	tool iota.Tool,
 	cloud iota.Cloud,
-) {
+) error {
+	oopsBuilder := oops.
+		Code("NewEnvironment").
+		With("tool", tool).
+		With("cloud", cloud)
+
 	switch tool {
 	case iota.Terraform:
-		general := 
 	case iota.Packer:
+		general := NewPackerGeneralEnvironment()
+	default:
+		err := oopsBuilder.
+			Errorf("unknown tool: %s", tool)
+
+		return err
 	}
+
+	return nil
 }
---
+++ environment/new_packer_aws_environment.go
@@ -0,0 +1,23 @@
+package environment
+
+type PackerAwsRequired struct {
+	AWS_ACCESS_KEY                     string
+	AWS_SECRET_KEY                     string
+	AWS_IAM_PROFILE                    string
+	AWS_USER_IDS                       []string
+	AWS_AMI_NAME                       string
+	AWS_INSTANCE_TYPE                  string
+	AWS_REGION                         string
+	AWS_EC2_AMI_NAME_FILTER            string
+	AWS_EC2_AMI_ROOT_DEVICE_TYPE       string
+	AWS_EC2_AMI_VIRTUALIZATION_TYPE    string
+	AWS_EC2_AMI_OWNERS                 []string
+	AWS_EC2_SSH_USERNAME               string
+	AWS_EC2_INSTANCE_USERNAME          string
+	AWS_EC2_INSTANCE_USERNAME_HOME     string
+	AWS_EC2_INSTANCE_USERNAME_PASSWORD string
+}
+
+type PackerAwsEnvironment struct {
+	Required *PackerAwsRequired
+}
