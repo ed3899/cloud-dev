0fbf5337f98f75f6730f39725d83f0610b932826
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 13:38:07 -0700

    check

---
+++ manager/set_manager_path_plugins.go
@@ -0,0 +1,20 @@
+package manager
+
+import (
+	"os"
+
+	"github.com/samber/oops"
+)
+
+func (m *Manager) SetPluginsEnvironmentVars() error {
+	oopsBuilder := oops.
+		Code("SetPluginsEnvironmentVars")
+
+	err := os.Setenv(m.Tool.PluginPathEnvironmentVariable(), m.Path.Plugins)
+	if err != nil {
+		return oopsBuilder.
+			Errorf("failed to set %s environment variable", m.Tool.PluginPathEnvironmentVariable())
+	}
+
+	return nil
+}
