c861b78df962b38265a75c927d24e32e6618007e
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 08:39:12 -0700

    check

--- environment/new_packer_general_environment.go
+++ environment/new_packer_general_environment.go
@@ -1,5 +1,31 @@
 package environment
 
-func NewPackerGeneralEnvironment() {
-	
+import "github.com/spf13/viper"
+
+type PackerGeneralRequired struct {
+	GIT_USERNAME string
+	GIT_EMAIL    string
+	ANSIBLE_TAGS []string
+}
+
+type PackerGeneralOptional struct {
+	GIT_HUB_PERSONAL_ACCESS_TOKEN_CLASSIC string
+}
+
+type PackerGeneralEnvironment struct {
+	Required *PackerGeneralRequired
+	Optional *PackerGeneralOptional
+}
+
+func NewPackerGeneralEnvironment() *PackerGeneralEnvironment {
+	return &PackerGeneralEnvironment{
+		Required: &PackerGeneralRequired{
+			GIT_USERNAME: viper.GetString("Git.Username"),
+			GIT_EMAIL:    viper.GetString("Git.Email"),
+			ANSIBLE_TAGS: viper.GetStringSlice("AMI.Tools"),
+		},
+		Optional: &PackerGeneralOptional{
+			GIT_HUB_PERSONAL_ACCESS_TOKEN_CLASSIC: viper.GetString("Git.Hub.PersonalAccessTokenClassic"),
+		},
+	}
 }
