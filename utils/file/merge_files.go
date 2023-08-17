c028d1e554cc9e27c721c2eb91b61c0eae0437f2
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 17:11:21 -0700

    check

--- utils/file/merge_files.go
+++ utils/file/merge_files.go
@@ -3,9 +3,7 @@ package file
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/samber/oops"
 )
@@ -13,74 +11,52 @@ import (
 func MergeFilesTo(
 	outputFileAbsPath string,
 	inputAbsFilePaths ...string,
-) (
-	err error,
-) {
-	var (
-		oopsBuilder = oops.Code("MergeFilesTo").
-				With("outputFileAbsPath", outputFileAbsPath).
-				With("inputAbsFilePaths", inputAbsFilePaths)
-
-		mergedFile    *os.File
-		inputFilePath string
-		inputFile     *os.File
-		scanner       *bufio.Scanner
-		line          string
-	)
+) error {
+	oopsBuilder := oops.
+		Code("MergeFilesTo").
+		In("utils").
+		In("file").
+		With("outputFileAbsPath", outputFileAbsPath).
+		With("inputAbsFilePaths", inputAbsFilePaths)
 
 	// Create a new file to write the merged content
-	if mergedFile, err = os.CreateTemp(filepath.Dir(outputFileAbsPath), filepath.Base(outputFileAbsPath)); err != nil {
-		err = oopsBuilder.Wrapf(err, "error creating merged file")
-		return
+	mergedFile, err := os.Create(outputFileAbsPath)
+	if err != nil {
+		err := oopsBuilder.Wrapf(err, "error creating merged file")
+
+		return err
 	}
-	defer func(mergedFile *os.File) {
-		if err := mergedFile.Close(); err != nil {
-			log.Fatalf(
-				"%+v",
-				oopsBuilder.
-					Wrapf(err, "error closing merged file: %s", mergedFile.Name()),
-			)
-		}
-	}(mergedFile)
+	defer mergedFile.Close()
 
-	for _, inputFilePath = range inputAbsFilePaths {
+	for _, inputFilePath := range inputAbsFilePaths {
 		// Open each file and append its content to the merged file
-		if inputFile, err = os.Open(inputFilePath); err != nil {
-			err = oopsBuilder.Wrapf(err, "error opening file %s", inputFilePath)
-			return
-		}
-		defer func(inputFile *os.File) {
-			if err := inputFile.Close(); err != nil {
-				log.Fatalf(
-					"%+v",
-					oopsBuilder.
-						Wrapf(err, "error closing file: %s", inputFile.Name()),
-				)
-			}
-		}(inputFile)
+		inputFile, err := os.Open(inputFilePath)
+		if err != nil {
+			err := oopsBuilder.Wrapf(err, "error opening file %s", inputFilePath)
 
-		scanner = bufio.NewScanner(inputFile)
+			return err
+		}
+		defer inputFile.Close()
+
+		scanner := bufio.NewScanner(inputFile)
 		for scanner.Scan() {
 			// Write each line to the merged file
-			line = scanner.Text()
-			if _, err = fmt.Fprintf(mergedFile, "%s\n", line); err != nil {
-				err = oopsBuilder.Wrapf(err, "error writing line '%s' to merged file", line)
-				return
+			line := scanner.Text()
+			_, err = fmt.Fprintf(mergedFile, "%s\n", line)
+			if err != nil {
+				err := oopsBuilder.Wrapf(err, "error writing line '%s' to merged file", line)
+
+				return err
 			}
 		}
 
-		if err = scanner.Err(); err != nil {
-			err = oopsBuilder.Wrapf(err, "error scanning file %s", inputFilePath)
-			return
+		err = scanner.Err()
+		if err != nil {
+			err := oopsBuilder.Wrapf(err, "error scanning file %s", inputFilePath)
+
+			return err
 		}
 	}
 
-	return
+	return nil
 }
-
-type MergeFilesToF func(
-	outputDirAbsPath string,
-	inputAbsFilePaths ...string,
-) (
-	err error,
-)
