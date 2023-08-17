fc81cba588d984057877ff0c92eb29f599ddb996
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-16 18:25:56 -0700

    check

--- utils/ip/read_ip_from_file.go
+++ utils/ip/read_ip_from_file.go
@@ -1,39 +1,36 @@
 package ip
 
 import (
-	"fmt"
 	"os"
 	"regexp"
 
 	"github.com/samber/oops"
 )
 
-func ReadIpFromFile(absPath string) (ip string, err error) {
-	var (
-		oopsBuilder = oops.
-				Code("read_ip_from_file_failed").
-				With("absPath", absPath)
-		// Define the regular expression pattern for matching an IP address
-		ipPattern = "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
-		// Compile the regular expression
-		ipRegex = regexp.MustCompile(ipPattern)
-
-		content []byte
-	)
+func ReadIpFromFile(absPath string) (string, error) {
+	oopsBuilder := oops.
+		Code("read_ip_from_file_failed").
+		With("absPath", absPath)
+	// Define the regular expression pattern for matching an IP address
+	ipPattern := "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
+	// Compile the regular expression
+	ipRegex := regexp.MustCompile(ipPattern)
 
 	// Read the contents of the file
-	if content, err = os.ReadFile(absPath); err != nil {
-		return "", fmt.Errorf("error reading file: %s", err)
+	content, err := os.ReadFile(absPath)
+	if err != nil {
+		err = oopsBuilder.
+			Wrapf(err, "error reading file")
+		return "", err
 	}
 
 	// Find the first match in the content
-	ip = ipRegex.FindString(string(content))
-
+	ip := ipRegex.FindString(string(content))
 	if len(ip) == 0 {
-		err = oopsBuilder.
+		err := oopsBuilder.
 			Errorf("no valid IP address found in file")
-		return
+		return "", err
 	}
 
-	return
+	return ip, nil
 }
