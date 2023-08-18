97cef2b438cedd6060c108004045156750d2f359
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-17 19:42:45 -0700

    check

---
+++ manager/clean.go
@@ -0,0 +1,17 @@
+package manager
+
+func (* Manager) Clean() error {
+	// Files
+	// 	- manifest.json
+	// 	- .auto.pkrvars.hcl
+	// 	- .auto.tfvars
+	// 	- .terraform.lock.hcl
+	// 	- terraform.tfstate
+	// 	- terraform.tfstate.backup
+
+	// Dirs
+	// 	- plugins
+	// 	- .terraform
+	
+	return nil
+}
