55b3502f9242ef2bf017872b57ab0dcd29e73105
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-18 10:23:23 -0700

    check

--- manager/clean.go
+++ manager/clean.go
@@ -7,30 +7,15 @@ import (
 	"github.com/samber/oops"
 )
 
-func (m *Manager) CleanPacker() error {
+func (m *Manager) Clean() error {
 	oopsBuilder := oops.
 		Code("Clean").
 		In("manager").
 		Tags("Manager")
 
-	if m.Tool.Iota() != iota.Packer {
-		err := oopsBuilder.
-			Errorf("tool is not packer")
-
-		return err
-	}
-
-	err := os.Remove(m.Path.PackerManifest)
+	err := os.Remove(m.Path.Executable)
 	if err != nil {
-		err := oopsBuilder.
-			Wrapf(err, "failed to remove packer manifest file")
-
-		return err
-	}
-
-	err = os.Remove(m.Path.Executable)
-	if err != nil {
-		err := oopsBuilder.
+		err = oopsBuilder.
 			Wrapf(err, "failed to remove executable")
 
 		return err
@@ -38,7 +23,7 @@ func (m *Manager) CleanPacker() error {
 
 	err = os.Remove(m.Path.Vars)
 	if err != nil {
-		err := oopsBuilder.
+		err = oopsBuilder.
 			Wrapf(err, "failed to remove vars")
 
 		return err
@@ -46,8 +31,50 @@ func (m *Manager) CleanPacker() error {
 
 	err = os.RemoveAll(m.Path.Dir.Plugins)
 	if err != nil {
-		err := oopsBuilder.
+		err = oopsBuilder.
 			Wrapf(err, "failed to remove plugins directory")
+
+		return err
+	}
+
+	switch m.Tool.Iota() {
+	case iota.Packer:
+		err = os.Remove(m.Path.PackerManifest)
+		if err != nil {
+			err = oopsBuilder.
+				Wrapf(err, "failed to remove packer manifest file")
+
+			return err
+		}
+
+	case iota.Terraform:
+		err = os.Remove(m.Path.Terraform.Lock)
+		if err != nil {
+			err = oopsBuilder.
+				Wrapf(err, "failed to remove terraform lock file")
+
+			return err
+		}
+
+		err = os.Remove(m.Path.Terraform.State)
+		if err != nil {
+			err = oopsBuilder.
+				Wrapf(err, "failed to remove terraform state file")
+
+			return err
+		}
+
+		err = os.Remove(m.Path.Terraform.Backup)
+		if err != nil {
+			err = oopsBuilder.
+				Wrapf(err, "failed to remove terraform backup file")
+
+			return err
+		}
+
+	default:
+		err = oopsBuilder.
+			Errorf("unknown tool: %#v", m.Tool)
 
 		return err
 	}
