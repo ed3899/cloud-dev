a7bf65d07b2ff82f2e0a0c5ff4a905da24af6dd1
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-18 10:55:06 -0700

    check

--- cmd/clean/packer.go
+++ cmd/clean/packer.go
@@ -1,14 +1,80 @@
 package clean
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+	"os"
+
+	"github.com/ed3899/kumo/common/iota"
+	"github.com/ed3899/kumo/manager"
+	"github.com/samber/oops"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 func Packer() *cobra.Command {
 	return &cobra.Command{
 		Use:   "packer",
 		Short: "Cleans up packer files.",
 		Long:  `Cleans up all the files created by packer under a specific cloud found in the kumo.config.yaml file.`,
-		Run: func(cmd *cobra.Command, args []string) {
+		PreRun: func(cmd *cobra.Command, args []string) {
+			oopsBuilder := oops.
+				Code("Packer").
+				In("cmd").
+				In("clean").
+				Tags("Cobra", "PreRun")
 
+			cwd, err := os.Getwd()
+			if err != nil {
+				log.Fatalf(
+					"%+v",
+					oopsBuilder.
+						Wrapf(err, "Error occurred while getting current working directory"),
+				)
+			}
+
+			viper.SetConfigName("kumo.config")
+			viper.SetConfigType("yaml")
+			viper.AddConfigPath(cwd)
+
+			err = viper.ReadInConfig()
+			if err != nil {
+				log.Fatalf(
+					"%+v",
+					oopsBuilder.
+						Wrapf(err, "Error occurred while reading config file. Make sure a kumo.config.yaml file exists in the current working directory"),
+				)
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			oopsBuilder := oops.
+				Code("Packer").
+				In("cmd").
+				Tags("cobra.Command").
+				With("command", *cmd).
+				With("args", args)
+
+			defer func() {
+				if r := recover(); r != nil {
+					err := oopsBuilder.Errorf("%v", r)
+					log.Fatalf("panic: %+v", err)
+				}
+			}()
+
+			_manager, err := manager.NewManager(iota.CloudIota(viper.GetString("cloud")), iota.Packer)
+			if err != nil {
+				err := oopsBuilder.
+					Wrapf(err, "failed to create new manager")
+
+				panic(err)
+			}
+
+			err = _manager.Clean()
+			if err != nil {
+				err := oopsBuilder.
+					Wrapf(err, "failed to clean up packer files")
+
+				panic(err)
+			}
 		},
 	}
 }
--- manager/clean.go
+++ manager/clean.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ed3899/kumo/common/iota"
 	"github.com/samber/oops"
+	"go.uber.org/zap"
 )
 
 func (m *Manager) Clean() error {
@@ -13,64 +14,58 @@ func (m *Manager) Clean() error {
 		In("manager").
 		Tags("Manager")
 
+	logger, _ := zap.NewProduction(
+		zap.AddCaller(),
+		zap.AddStacktrace(
+			zap.ErrorLevel,
+		),
+	)
+	defer logger.Sync()
+
 	err := os.Remove(m.Path.Executable)
 	if err != nil {
-		err = oopsBuilder.
-			Wrapf(err, "failed to remove executable")
-
-		return err
+		logger.Error("failed to remove executable", zap.Error(err))
 	}
+	logger.Info("removed executable", zap.String("path", m.Path.Executable))
 
 	err = os.Remove(m.Path.Vars)
 	if err != nil {
-		err = oopsBuilder.
-			Wrapf(err, "failed to remove vars")
-
-		return err
+		logger.Error("failed to remove vars file", zap.Error(err))
 	}
+	logger.Info("removed vars file", zap.String("path", m.Path.Vars))
 
 	err = os.RemoveAll(m.Path.Dir.Plugins)
 	if err != nil {
-		err = oopsBuilder.
-			Wrapf(err, "failed to remove plugins directory")
-
-		return err
+		logger.Error("failed to remove plugins directory", zap.Error(err))
 	}
+	logger.Info("removed plugins directory", zap.String("path", m.Path.Dir.Plugins))
 
 	switch m.Tool.Iota() {
 	case iota.Packer:
 		err = os.Remove(m.Path.PackerManifest)
 		if err != nil {
-			err = oopsBuilder.
-				Wrapf(err, "failed to remove packer manifest file")
-
-			return err
+			logger.Error("failed to remove packer manifest file", zap.Error(err))
 		}
+		logger.Info("removed packer manifest file", zap.String("path", m.Path.PackerManifest))
 
 	case iota.Terraform:
 		err = os.Remove(m.Path.Terraform.Lock)
 		if err != nil {
-			err = oopsBuilder.
-				Wrapf(err, "failed to remove terraform lock file")
-
-			return err
+			logger.Error("failed to remove terraform lock file", zap.Error(err))
 		}
+		logger.Info("removed terraform lock file", zap.String("path", m.Path.Terraform.Lock))
 
 		err = os.Remove(m.Path.Terraform.State)
 		if err != nil {
-			err = oopsBuilder.
-				Wrapf(err, "failed to remove terraform state file")
-
-			return err
+			logger.Error("failed to remove terraform state file", zap.Error(err))
 		}
+		logger.Info("removed terraform state file", zap.String("path", m.Path.Terraform.State))
 
 		err = os.Remove(m.Path.Terraform.Backup)
 		if err != nil {
-			err = oopsBuilder.
-				Wrapf(err, "failed to remove terraform backup file")
-
-			return err
+			logger.Error("failed to remove terraform backup file", zap.Error(err))
 		}
+		logger.Info("removed terraform backup file", zap.String("path", m.Path.Terraform.Backup))
 
 	default:
 		err = oopsBuilder.
