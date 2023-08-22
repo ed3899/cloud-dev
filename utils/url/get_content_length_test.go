71908c9c4f9b9e5c8e3f5a12e09b24712ddfb5a9
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-08-22 12:38:21 -0700

    added tests for get-content-length

---
+++ utils/url/get_content_length_test.go
@@ -0,0 +1,32 @@
+package url
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("GetContentLength", func() {
+	Context("when the url is valid", func() {
+		It("should return content length and no error for successful request", func() {
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Length", "12345")
+				w.WriteHeader(http.StatusOK)
+			}))
+			defer server.Close()
+
+			contentLength, err := GetContentLength(server.URL)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contentLength).To(Equal(int64(12345)))
+		})
+	})
+
+	Context("when the url is not valid", func() {
+		It("should return error for unsuccessful request", func() {
+			_, err := GetContentLength("non-existent-url")
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
