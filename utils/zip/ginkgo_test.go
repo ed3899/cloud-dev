03a58351c19414968e34ba57e7e3c77b17c2960c
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2023-08-22 10:56:55 -0700

    Ed3899/issue99 (#100)

---
+++ utils/zip/ginkgo_test.go
@@ -0,0 +1,13 @@
+package zip
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestZip(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Zip Suite")
+}
